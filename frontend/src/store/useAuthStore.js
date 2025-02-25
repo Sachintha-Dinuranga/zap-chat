59312cc10e535071458f68ea7989795ee6807fde
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2025-02-26 00:28:01 +0530

    fix signup function and add navbar

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,6 +2,7 @@ import { create } from "zustand";
 import { instance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 export const useAuthStore = create((set) => ({
+  // Initial states
   authUser: null,
   isSigninUp: false,
   isLoggingIng: false,
@@ -10,6 +11,7 @@ export const useAuthStore = create((set) => ({
 
   checkAuth: async () => {
     try {
+      // Send a get request to the end point
       const res = await instance.get("/auth/check");
 
       set({ authUser: res.data });
@@ -24,7 +26,7 @@ export const useAuthStore = create((set) => ({
   signup: async (data) => {
     set({ isSigningUp: true });
     try {
-      const res = await axiosInstance.post("/auth/signup", data);
+      const res = await instance.post("/auth/signup", data);
       set({ authUser: res.data });
       toast.success("Account created successfully");
       get().connectSocket();
@@ -32,6 +34,16 @@ export const useAuthStore = create((set) => ({
       toast.error(error.response.data.message);
     } finally {
       set({ isSigningUp: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await instance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
     }
   },
 }));
