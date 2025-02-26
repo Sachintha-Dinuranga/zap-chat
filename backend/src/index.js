54f99e361e943f67eddfa4d71114390e97381d67
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2025-02-26 13:26:56 +0530

    create sidebar component

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,7 @@ app.use(
 
 // Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 // connect to the server and db
 app.listen(port, () => {
